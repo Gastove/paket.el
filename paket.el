66b8d539e78535b279c03d84367fd95f7e44d0dc
Martyn Osborne <zzdtri@live.co.uk>
2015-04-20 19:32:34 +0100

    basic start

---
+++ paket.el
@@ -0,0 +1,37 @@
+
+(require 'compile)
+
+(defgroup paket nil
+  "Paket tooling for Emacs"
+  :prefix "paket-")
+
+(defconst paket-buffer-name "*paket*"
+  "The buffer for Paket results")
+
+(defcustom paket-program-name "paket"
+  "The shell command for PAket"
+  :type 'string)
+
+(define-compilation-mode paket-buffer-mode "Paket"
+  "Paket buffer mode.")
+
+(defun paket-send-command (command)
+  (with-current-buffer
+      (compilation-start command
+                         nil
+                         (lambda (x) paket-buffer-name))))
+
+(defun paket-install ()
+  (interactive)
+  (paket-send-command "paket install --hard"))
+
+(defun paket-add-nuget (package)
+  (interactive
+   (list
+    (read-string "Package name:")))
+  (paket-send-command (concat "paket add nuget " package)))
+
+(define-minor-mode paket-mode
+  "Mode for interacting with Paket."
+  :group 'paket
+  :lighter " paket")
