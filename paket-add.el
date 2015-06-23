cff59fc452b1dc346c827a08bf43ac70b418adf9
Martyn Osborne <zzdtri@live.co.uk>
2015-06-23 22:35:29 +0100

    way simpler paket-add-nuget completion.  isn't IDO, but works

--- paket-add.el
+++ paket-add.el
@@ -4,33 +4,15 @@
   (let ((executable (paket-find-executable paket-executable)))
     (start-file-process "paket" paket-results-buffer executable "find-packages" "searchtext" text "-s" "max" "100000")))
 
-(defun paket--find-packages-filter (proc)
-  (set-process-filter
-   proc
-   (lambda (proc str)
-     (let ((old-result package-cache))
-       (setq package-cache (concat str old-result))))))
-
-(defvar package-list (list))
-(defvar cache-loaded nil)
-
-(defun paket--fetch-packages (search)
-  (setq package-cache "")
-  (let ((process (paket--start-package-process search)))
-    (paket--find-packages-filter process)
-    (set-process-sentinel
-     process
-     (lambda (p e)
-       (if (string-match-p "finished" e)
-           (exit-recursive-edit))))
-    (recursive-edit))
-  (split-string package-cache "\n"))
+(defun paket--search-packages (search)
+  (let ((executable (paket-find-executable paket-executable)))
+    (process-lines executable "find-packages" "searchtext" search "-s")))
 
 (defun paket-add-nuget (package)
   "Add a Nuget PACKAGE with Paket."
   (interactive
    (list
-    (completing-read "Package name:" (completion-table-dynamic 'paket--fetch-packages))))
+    (completing-read "Package name: " (completion-table-with-cache 'paket--search-packages))))
   (paket--send-command (concat "add nuget " package)))
 
 (provide 'paket-add)
